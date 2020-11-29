abd42d0fe854101143462d97da75bef040384260
JungHyukJin <hyukjin4976@naver.com>
2020-11-29 21:41:55 +0900

    CSS: work done

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -28,7 +28,7 @@ function Movie({ id, year, title, summary, poster, genres }) {
               </li>
             ))}
           </ul>
-          <p className="movie__summary">{summary}</p>
+          <p className="movie__summary">{summary.slice(0, 300)}...</p>
         </div>
       </div>
     </Link>
